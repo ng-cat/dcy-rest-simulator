ecbed8c69cc36775c53789ea5c165e655729552a
cTantalo <christian.tantalo@decisyon.com>
2024-04-10 15:00:15 +0200

    added some scenarios

--- routerServer.js
+++ routerServer.js
@@ -1,66 +1,82 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const routerServer = express.Router();
-var cors = require('cors');
-var bodyParser = require('body-parser')
-var fs = require('fs');
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var fs = require("fs");
 
-    routerServer.use(cors());
-    routerServer.use( bodyParser.json() );
-    routerServer.use(bodyParser.urlencoded({
-      extended: true
-    }));
+routerServer.use(cors());
+routerServer.use(bodyParser.json());
+routerServer.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-    routerServer.get('/', (req,res)=>{
-        res.send({message: 'Ciao'});
-    });
+routerServer.get("/", (req, res) => {
+  res.send({ message: "Ciao" });
+});
 
-    routerServer.get('/report', (req,res)=>{
-        var mock = fs.readFileSync('./scenarios/report.json');
-        var data = JSON.parse(mock);
-        setTimeout(() => {
-            res.json(data);
-        }, 5000);
-    });
+routerServer.get("/report", (req, res) => {
+  var mock = fs.readFileSync("./scenarios/report.json");
+  var data = JSON.parse(mock);
+  setTimeout(() => {
+    res.json(data);
+  }, 5000);
+});
 
-    routerServer.get('/api/v2/rest/application/:model/report/:report', (req,res)=>{
-        let model = req.params.model;
-        let report = req.params.report;
-        var mock = fs.readFileSync('./scenarios/' + model + '-' + report + '.json');
-        var data = JSON.parse(mock);
-        setTimeout(() => {
-            res.json(data);
-        }, 0);
-    });
+routerServer.get(
+  "/api/v2/rest/application/:model/report/:report",
+  (req, res) => {
+    let model = req.params.model;
+    let report = req.params.report;
+    var mock = fs.readFileSync("./scenarios/" + model + "-" + report + ".json");
+    var data = JSON.parse(mock);
+    setTimeout(() => {
+      res.json(data);
+    }, 0);
+  }
+);
 
-    routerServer.get('/api/rest/application/:model/report/:report', (req,res)=>{
-        let model = req.params.model;
-        let report = req.params.report;
-        var mock = fs.readFileSync('./scenarios/' + model + '-' + report + '.json');
-        var data = JSON.parse(mock);
-        setTimeout(() => {
-            res.json(data);
-        }, 0);
-    });
+routerServer.get("/api/rest/application/:model/report/:report", (req, res) => {
+  let model = req.params.model;
+  let report = req.params.report;
+  var mock = fs.readFileSync("./scenarios/" + model + "-" + report + ".json");
+  var data = JSON.parse(mock);
+  setTimeout(() => {
+    res.json(data);
+  }, 0);
+});
 
-    routerServer.get('/api/rest/application/:model/page/:page/report/:report', (req,res)=>{
-        let model = req.params.model;
-        let report = req.params.report;
-        var mock = fs.readFileSync('./scenarios/' + model + '-' + report + '.json');
-        var data = JSON.parse(mock);
-        setTimeout(() => {
-            res.json(data);
-        }, 0);
-    });
+routerServer.get(
+  "/api/rest/application/:model/page/:page/report/:report",
+  (req, res) => {
+    let model = req.params.model;
+    let report = req.params.report;
+    var mock = fs.readFileSync("./scenarios/" + model + "-" + report + ".json");
+    var data = JSON.parse(mock);
+    setTimeout(() => {
+      res.json(data);
+    }, 0);
+  }
+);
 
-    routerServer.get('/Orders', (req,res)=>{
-        var mock = fs.readFileSync('./scenarios/orders.json');
-        var data = JSON.parse(mock);
-        setTimeout(() => {
-            res.json(data);
-        }, 10000);
-    });
+routerServer.get("/Orders", (req, res) => {
+  var mock = fs.readFileSync("./scenarios/orders.json");
+  var data = JSON.parse(mock);
+  setTimeout(() => {
+    res.json(data);
+  }, 10000);
+});
 
+routerServer.post("/ALMC/:fileName", (req, res) => {
+  let fileName = req.params.fileName;
+  var mock = fs.readFileSync("./scenarios/ALMC/" + fileName + ".json");
+  var data = JSON.parse(mock);
+  setTimeout(() => {
+    res.json(data);
+  }, 0);
+});
 
 module.exports = routerServer;
