5490c96a242a64eac23c27d63f26544d295f9e55
cTantalo <christian.tantalo@decisyon.com>
2025-07-08 10:28:13 +0200

    Added other scenarios

--- routerServer.js
+++ routerServer.js
@@ -26,6 +26,26 @@ routerServer.get("/report", (req, res) => {
   }, 5000);
 });
 
+routerServer.post("/cassiopeaWeb/report/do.cas", (req, res) => {
+  const revent = req.params.revent;
+  const csMLO = req.params.csMLO;
+  const idReportRequest = req.body.idReportRequest;
+  try {
+    var mock = fs.readFileSync(
+      "./scenarios/contextMenu/" + idReportRequest.split("CTXSEP")[1] + ".js"
+    );
+    console.log("Mock file loaded:", idReportRequest);
+  } catch (e) {
+    var mock = fs.readFileSync("./scenarios/contextMenu/menu.js");
+    console.log(
+      "Mock file not found: " + idReportRequest + ", using default menu.js"
+    );
+  }
+  setTimeout(() => {
+    res.send(mock.toString());
+  }, 0);
+});
+
 routerServer.get(
   "/api/v2/rest/application/:model/report/:report",
   (req, res) => {
@@ -50,11 +70,39 @@ routerServer.get("/api/rest/application/:model/report/:report", (req, res) => {
 });
 
 routerServer.get(
+  "/api/v2/rest/application/:model/report/:report/columns_definition",
+  (req, res) => {
+    let model = req.params.model;
+    let report = req.params.report;
+    var mock = fs.readFileSync(
+      "./scenarios/columns_definition/" + model + "-" + report + ".json"
+    );
+    var data = JSON.parse(mock);
+    setTimeout(() => {
+      res.json(data);
+    }, 0);
+  }
+);
+
+routerServer.get(
   "/api/rest/application/:model/page/:page/report/:report",
   (req, res) => {
     let model = req.params.model;
     let report = req.params.report;
     var mock = fs.readFileSync("./scenarios/" + model + "-" + report + ".json");
+    var data = JSON.parse(mock);
+    setTimeout(() => {
+      res.json(data);
+    }, 0);
+  }
+);
+
+routerServer.get(
+  "/api/rest/user/:userId/application/:application/page/:page/widget/:widget/settings",
+  (req, res) => {
+    let model = req.params.model;
+    let report = req.params.report;
+    var mock = fs.readFileSync("./scenarios/settings/settings.json");
     var data = JSON.parse(mock);
     setTimeout(() => {
       res.json(data);
